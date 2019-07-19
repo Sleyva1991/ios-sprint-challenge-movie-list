ebdee5726606291075fc6b16984a0f1546253b4b
Steven <Sleyva1991@outlook.com>
2019-07-19 13:05:28 -0400

    Added Moviecontroller, inputed coded, also addeded MovieViewController and MovieTableViewController files

---
+++ Movie List/Controller/MovieController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieController.swift
+//  Movie List
+//
+//  Created by Steven Leyva on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class MovieController {
+    
+    var movies: [Movie] = []
+    
+    init() {
+        createMovie(with: "Star Wars: The Rise of Skywalker")
+    }
+    
+    func createMovie(with text: String) {
+        let movie = Movie(text: text)
+        
+        movies.append(movie)
+    }
+    
+////    func toggleIsLiked(for movie: Movie) {
+////        guard let indx = movies.firstIndex(of: movie) else { return }
+////        movies[index].seen = !movie.seen
+//    }
+}
---
+++ Movie List/View Controller/MovieViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MovieViewController.swift
+//  Movie List
+//
+//  Created by Steven Leyva on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+ 
+}
